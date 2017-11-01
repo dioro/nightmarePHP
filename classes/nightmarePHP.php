4697ee47b3fb6aa0765121d9e7b34d3336bede0e
vagrant <nino@skvorc.org>
2017-11-01 10:32:25 +0000

    added experimental methods

--- classes/nightmarePHP.php
+++ classes/nightmarePHP.php
@@ -11,6 +11,8 @@ namespace Rayac\nightmarePHP;
 
 class nightmarePHP
 {
+    private $tempname;
+
     private $nodeCode;
     private $result;
 
@@ -41,9 +43,12 @@ class nightmarePHP
      */
     public function getCode()
     {
-        return $this->nodeCode;
+        if($this->raw == true) {
+            $this->nodeCode .= ";";
+        }
+        return trim($this->nodeCode);
     }
-    
+
 
     /**
      * Runs the code
@@ -51,22 +56,86 @@ class nightmarePHP
      */
     public function run()
     {
+        if($this->raw == true) {
+            $this->nodeCode .= ";";
+        }
+
         //creating temp file and storing nightmareJS code in it
-        $tempname = tempnam("../", "jej");
-        $temp = fopen($tempname, "r+");
+        $this->tempname = tempnam("../", "jej");
+        $temp = fopen($this->tempname, "r+");
         fwrite($temp, $this->nodeCode);
 
         //execution of code
-        exec("xvfb-run node " . $tempname, $output);
+        exec("xvfb-run node " . $this->tempname, $output);
+
+        //Used for debugging
+        //Dumps temp file contents that has nightmareJS
+//        fseek($temp, 0);
+//        dump(fread($temp, 1024));
 
         //cleanup
         fclose($temp);
-        unlink($tempname);
+        unlink($this->tempname);
 
         $this->result = $output;
 
         return $this;
     }
+
+    public function config($config)
+    {
+        $this->raw = true;
+
+        $this->nodeCode = "
+const Nightmare = require(\"nightmare\");
+const nightmare = Nightmare({". $config. " });
+        
+nightmare";
+    }
+
+    public function _goto($url)
+    {
+        $this->nodeCode .= "\n.goto('".$url."')";
+        return $this;
+    }
+
+    public function type($selector, $text = null)
+    {
+        $this->nodeCode .= "\n.type('".$selector."', '".$text."')";
+        return $this;
+    }
+
+    public function wait($for)
+    {
+        $this->nodeCode .= "\n.wait('".$for."')";
+        return $this;
+    }
+
+    public function evaluate($evaluate)
+    {
+        $this->nodeCode .= "\n.evaluate(".$evaluate.")";
+        return $this;
+    }
+
+    public function end($end = null)
+    {
+        $this->nodeCode .= "\n.end(".$end.")";
+        return $this;
+    }
+
+    public function then($then)
+    {
+        $this->nodeCode .= "\n.then(".$then.")";
+        return $this;
+    }
+
+    public function _catch($catch)
+    {
+        $this->nodeCode .= "\n.catch(".$catch.")";
+        return $this;
+    }
+
+
 
     public function getResult()
     {
--- public/index.php
+++ public/index.php
@@ -6,23 +6,37 @@ require_once '../vendor/autoload.php';
 
 
 
+//$testing = new nightmarePHP();
+//$testing->rawInput("
+//const Nightmare = require(\"nightmare\");
+//const nightmare = Nightmare({ show: false });
+//
+//nightmare
+//    .goto('https://www.reddit.com/r/news/')
+//    .wait('#siteTable > div:first-child .title > a')
+//    .evaluate(() => document.querySelector(\"#siteTable > div:first-child .title > a\").textContent)
+//.end()
+//    .then(console.log)
+//    .catch((error) => {
+//    console.error('Search failed:', error);
+//});
+//")->run();
+
 $testing = new nightmarePHP();
-$testing->rawInput("
-const Nightmare = require(\"nightmare\");
-const nightmare = Nightmare({ show: false });
 
-nightmare
-    .goto('https://www.reddit.com/r/news/')
-    .wait('#siteTable > div:first-child .title > a')
-    .evaluate(() => document.querySelector(\"#siteTable > div:first-child .title > a\").textContent)
-.end()
-    .then(console.log)
-    .catch((error) => {
-    console.error('Search failed:', error);
-});")->run();
+$testing->config("show: false");
 
+$testing->_goto('https://www.reddit.com/r/news/')
+        ->wait('#siteTable > div:first-child .title > a')
+        ->evaluate("() => document.querySelector(\"#siteTable > div:first-child .title > a\").textContent")
+        ->end()
+        ->then("console.log")
+        ->_catch("(error) => {console.error('Search failed:', error);}")->run();
 
 dump($testing->getResult());
+
+
+
 
 //fseek($temp, 0);
 //dump(fread($temp, 1024));
